90e7c1a70e1ec1944bb5a5f2b583a5d058124151
bobswiftfoot <natepfau@yahoo.com>
2021-09-26 13:28:39 -0500

    Added Queries and Mutiations in client

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -46,7 +46,7 @@ const typeDefs = gql`
   type Mutation 
   {
     login(email: String!, password: String!): Auth
-    createUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(content: BookContent!): User
     removeBook(bookId: String!): User
   }
